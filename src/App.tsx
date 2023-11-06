a4d965079d49204408b668dd0ba56f622666ec6d
Kishan Mandora <kishanmandora7@gmail.com>
2023-11-07 04:17:46 +0530

    styles: setup design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Earth</h1>;
+  return (
+    <h1 className="text-3xl text-neutral-50 bg-neutral-1000 h-screen flex justify-center items-center">
+      100x
+    </h1>
+  );
 }
 
 export default App;
